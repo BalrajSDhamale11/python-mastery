58ad3347eea05b89e67c788e3fa974d752ed6b1f
Balraj <balrajdhamale112005@gmail.com>
2026-03-28 00:31:40 +0530

    Add: the_input_cleaner - string_methods

---
+++ 04_string_methods/the_input_cleaner.py
@@ -0,0 +1,17 @@
+email = input("Enter email: ")
+username = input("Enter username: ")
+cleaned_email = email.strip().lower()
+cleaned_username = username.strip()
+if '_' in username:         #has_underscore = username.count('_') > 0 
+    has_underscore = True   #has_underscore = username.find('_') != -1
+else:
+    has_underscore = False
+if cleaned_username.startswith("raj"):
+    starts_with_raj = True
+email_length = len(cleaned_email)
+
+print(f"Cleaned Email    : {cleaned_email}")
+print(f"Cleaned Username : {cleaned_username}")
+print(f"Has underscore   : {has_underscore}")
+print(f"Starts with raj  : {starts_with_raj}")
+print(f"Email length     : {email_length}")
