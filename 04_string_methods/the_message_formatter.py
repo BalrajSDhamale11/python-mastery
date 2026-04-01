0d6121bf9ad0717bf74ac1071f223676e664313e
Balraj <balrajdhamale112005@gmail.com>
2026-04-01 14:17:13 +0530

    Add: the_message_formatter - string_methods

---
+++ 04_string_methods/the_message_formatter.py
@@ -0,0 +1,22 @@
+message = input("Enter your message: ")
+original = message
+cleaned_m = message.strip()
+uppercase_m = cleaned_m.upper()
+split = cleaned_m.split()
+word_count = len(split)
+contains_raj = 'raj' in cleaned_m
+startswith_hello = cleaned_m.startswith('hello')
+endswith_raj = cleaned_m.endswith('raj!')
+replace = cleaned_m.replace('raj', 'amit')
+
+print("\n===== Message Report =====\n")
+
+print(f"Original               : {original}")
+print(f"Cleaned                : {cleaned_m}")
+print(f"Uppercase              : {uppercase_m}")
+print(f"Word Count             : {word_count}")
+print(f"Contains 'raj'         : {contains_raj}")
+print(f"Starts with 'hello'    : {startswith_hello}")
+print(f"Ends with 'raj!'       : {endswith_raj}")
+print(f"Replace 'raj' → 'amit' : {replace}")
+print("\n==========================")
---
+++ practice.py
@@ -0,0 +1,2 @@
+cleaned_email = input("Enter your email: ").strip().lower()
+print(f"Clean_email: {cleaned_email}")
