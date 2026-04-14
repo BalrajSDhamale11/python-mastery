5ce283f4fd02151239345691f1eddd42812c2ae0
Balraj <balrajdhamale112005@gmail.com>
2026-04-14 12:21:39 +0530

    Add: if/else - the_age_gate

---
+++ 06_if/else/the_age_gate.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+#top-down from highest to lowest:
+if age >= 60:                          ## highest first
+    print("You are a Senoir Citizen.")
+elif age >= 18:
+    print("You are an Adult.")
+elif age >= 13:
+    print("You are a Teenager.")
+else:                                  # catches everything remaining
+    print("You are a Child.") 
